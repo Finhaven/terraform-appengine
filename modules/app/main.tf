aae4af0a7d2374a5f1a045848ca7613cf732b3b0
Aaron Cimolini <aaron@cimolini.com>
2023-04-08 13:57:08 -0700

    Minor refactoring.

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,17 +1,34 @@
+# Collect the different regions available within GCP.
+data "google_compute_regions" "available" {
+  project = var.project_id
+}
+
 resource "google_app_engine_application" "appengine_app" {
-  project        = var.project
-  location_id    = contains(data.google_compute_regions.available.names, var.location_id) == true ? var.location_id : "This is not one of the available locations in GCP."
+  # Ensure valid location has been passed.
+  location_id = contains(
+    data.google_compute_regions.available.names,
+    var.location_id
+  ) == true ? var.location_id : "This is not one of the available locations in GCP."
+
+  project        = var.project_id
   auth_domain    = var.auth_domain
   database_type  = var.database_type
   serving_status = var.serving_status
+
+  # Conditional dynamic blocks using the splat operator.
+  # https://developer.hashicorp.com/terraform/language/expressions/splat#single-values-as-lists
+
   dynamic "feature_settings" {
     for_each = var.feature_settings[*]
+
     content {
       split_health_checks = feature_settings.value.split_health_checks
     }
   }
+
   dynamic "iap" {
     for_each = var.iap[*]
+
     content {
       oauth2_client_id     = iap.value.oauth2_client_id
       oauth2_client_secret = iap.value.oauth2_client_secret

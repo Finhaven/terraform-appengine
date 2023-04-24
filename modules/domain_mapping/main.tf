ab3720ea6e28759768cb51ae701bd9522acb8298
Aaron Cimolini <aaron@cimolini.com>
2023-04-24 13:02:54 -0700

    Adds project_id variable. Cleans up some of the code.

--- modules/domain_mapping/main.tf
+++ modules/domain_mapping/main.tf
@@ -1,11 +1,14 @@
 resource "google_app_engine_domain_mapping" "domain_mapping" {
-  domain_name = var.domain_name
+  domain_name       = var.domain_name
+  project           = var.project_id
+  override_strategy = var.override_strategy
+
   dynamic "ssl_settings" {
     for_each = var.ssl_settings[*]
+
     content {
-      certificate_id                 = ssl_settings.value.certificate_id
-      ssl_management_type            = ssl_settings.value.ssl_management_type
+      certificate_id      = ssl_settings.value.certificate_id
+      ssl_management_type = ssl_settings.value.ssl_management_type
     }
   }
-  override_strategy = var.override_strategy
 }
--- modules/domain_mapping/variables.tf
+++ modules/domain_mapping/variables.tf
@@ -1,19 +1,22 @@
 variable "domain_name" {
   description = "(Required) Relative name of the domain serving the application."
   type        = string
-  default     = ""
+}
+
+variable "project_id" {
+  description = "ID of the project in which to create this domain mapping."
+  type        = string
 }
 
 variable "ssl_settings" {
-  description = ""
   type = object({
-    certificate_id                 = string,
-    ssl_management_type            = string,
+    certificate_id      = string,
+    ssl_management_type = string,
   })
   default = null
 
   validation {
-    condition     = var.ssl_settings != null ? ! contains([for ssl_management_type in var.ssl_settings[*].ssl_management_type : (ssl_management_type == null || contains(["AUTOMATIC", "MANUAL"], ssl_management_type)) if ssl_management_type != null], false) : true
+    condition     = var.ssl_settings != null ? !contains([for ssl_management_type in var.ssl_settings[*].ssl_management_type : (ssl_management_type == null || contains(["AUTOMATIC", "MANUAL"], ssl_management_type)) if ssl_management_type != null], false) : true
     error_message = "SSL management type must be one of [AUTOMATIC, MANUAL]."
   }
 }
@@ -21,10 +24,10 @@ variable "ssl_settings" {
 variable "override_strategy" {
   description = "(Optional) Whether the domain creation should override any existing mappings for this domain. By default, overrides are rejected. Possible values are STRICT and OVERRIDE."
   type        = string
-  default     = null
+  default     = "STRICT"
 
   validation {
-    condition     = var.override_strategy != null ? contains(["STRICT", "OVERRIDE"],var.override_strategy) : true
+    condition     = contains(["STRICT", "OVERRIDE"], var.override_strategy)
     error_message = "The override strategy field must be one of [STRICT, OVERRIDE]."
   }
 }

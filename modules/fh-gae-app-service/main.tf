c6d5b5000d15e05bd6bac5fb372923fd30416478
Aaron Cimolini <aaron@cimolini.com>
2023-08-08 19:04:56 -0700

    Replaces common_args var with more specific vars with proper types.

--- modules/fh-gae-app-service/main.tf
+++ modules/fh-gae-app-service/main.tf
@@ -13,11 +13,16 @@ module "deployment_state" {
 module "app_versions" {
   source = "../standard/multi-version"
 
-  versions = module.deployment_state.versions
-
-  # Passing untyped objects through Terragrunt results in JSON strings in the
-  # variables. Using jsondecode to rehydrate the objects.
-  common_args = jsondecode(var.common_args)
+  versions                  = module.deployment_state.versions
+  project_id                = var.project_id
+  service                   = var.service_name
+  service_account           = var.service_account
+  runtime                   = var.runtime
+  env_variables             = var.env_variables
+  noop_on_destroy           = var.noop_on_destroy
+  delete_service_on_destroy = var.delete_service_on_destroy
+  entrypoint                = var.entrypoint
+  vpc_access_connector      = var.vpc_access_connector
 }
 
 module "split_traffic" {
@@ -30,8 +35,8 @@ module "split_traffic" {
 
   # Leave these settings alone, unless you want to do some testing and
   # debugging. Deploys would fail if I used any other values. I never figured
-  # out why different settings would work. - Aaron Cimolini
-  # <aaron.cimolini@finhaven.com>
+  # out why different settings would not work.
+  # - Aaron Cimolini <aaron.cimolini@finhaven.com>
   shard_by        = "IP"
   migrate_traffic = false
 }
--- modules/fh-gae-app-service/variables.tf
+++ modules/fh-gae-app-service/variables.tf
@@ -6,19 +6,16 @@
 
 variable "project_id" {
   description = "ID of project in which to create the split traffic."
-}
-
-variable "service_name" {
-  description = "(Required) The name of the service these settings apply to."
   type        = string
-}
-
-variable "common_args" {
-  description = "Map of common args to pass to the child module for all versions."
 }
 
 variable "artifact_bucket_name" {
   description = "Name of the bucket where the deployment artifacts and state files are kept."
+  type        = string
+}
+
+variable "service_name" {
+  description = "(Required) The name of the service these settings apply to."
   type        = string
 }
 
@@ -32,4 +29,49 @@ variable "initial_code_zip_path" {
   type        = string
 }
 
+variable "runtime" {
+  description = "The GAE runtime to use for the service."
+  type        = string
+}
+
 # Optional Variables ##########################################################
+
+variable "service_account" {
+  description = "The identity that the deployed version will run as. Admin API will use the App Engine Appspot service account as default if this field is neither provided in app.yaml file nor through CLI flag."
+  type        = string
+  default     = null
+}
+
+variable "entrypoint" {
+  description = "(Optional) The entrypoint for the application."
+  type = object({
+    shell = string
+  })
+  default = null
+}
+
+variable "vpc_access_connector" {
+  description = "(Optional) Enables VPC connectivity for standard apps."
+  type = object({
+    name = string
+  })
+  default = null
+}
+
+variable "env_variables" {
+  description = "(Optional) Environment variables to be passed to the App Engine service"
+  type        = map(any)
+  default     = {}
+}
+
+variable "noop_on_destroy" {
+  description = "(Optional; Default: True)If set to true, the application version will not be deleted upon running Terraform destroy."
+  type        = bool
+  default     = true
+}
+
+variable "delete_service_on_destroy" {
+  description = "(Optional; Default: False)If set to true, the service will be deleted if it is the last version."
+  type        = bool
+  default     = false
+}

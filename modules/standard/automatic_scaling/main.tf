0f0d755e4d6bc8b55c73d95fef3196a7f256f1c4
Aaron Cimolini <aaron@cimolini.com>
2023-08-16 17:55:52 -0700

    Disabling handlers since we're not using them.

--- modules/standard/automatic_scaling/main.tf
+++ modules/standard/automatic_scaling/main.tf
@@ -35,39 +35,39 @@ resource "google_app_engine_standard_app_version" "appengine_standard" {
     }
   }
 
-  dynamic "handlers" {
-    for_each = var.handlers == null ? [] : var.handlers
+  # dynamic "handlers" {
+  #   for_each = var.handlers == null ? [] : var.handlers
 
-    content {
-      url_regex                   = var.handlers[handlers.key]["url_regex"]
-      security_level              = var.handlers[handlers.key]["security_level"]
-      login                       = var.handlers[handlers.key]["login"]
-      auth_fail_action            = var.handlers[handlers.key]["auth_fail_action"]
-      redirect_http_response_code = var.handlers[handlers.key]["redirect_http_response_code"]
+  #   content {
+  #     url_regex                   = var.handlers[handlers.key]["url_regex"]
+  #     security_level              = var.handlers[handlers.key]["security_level"]
+  #     login                       = var.handlers[handlers.key]["login"]
+  #     auth_fail_action            = var.handlers[handlers.key]["auth_fail_action"]
+  #     redirect_http_response_code = var.handlers[handlers.key]["redirect_http_response_code"]
 
-      dynamic "script" {
-        for_each = handlers.value.script[*]
+  #     dynamic "script" {
+  #       for_each = handlers.value.script[*]
 
-        content {
-          script_path = script.value.script_path
-        }
-      }
+  #       content {
+  #         script_path = script.value.script_path
+  #       }
+  #     }
 
-      dynamic "static_files" {
-        for_each = handlers.value.static_files[*]
+  #     dynamic "static_files" {
+  #       for_each = handlers.value.static_files[*]
 
-        content {
-          path                  = static_files.value.path
-          upload_path_regex     = static_files.value.upload_path_regex
-          http_headers          = static_files.value.http_headers
-          mime_type             = static_files.value.mime_type
-          expiration            = static_files.value.expiration
-          require_matching_file = static_files.value.require_matching_file
-          application_readable  = static_files.value.application_readable
-        }
-      }
-    }
-  }
+  #       content {
+  #         path                  = static_files.value.path
+  #         upload_path_regex     = static_files.value.upload_path_regex
+  #         http_headers          = static_files.value.http_headers
+  #         mime_type             = static_files.value.mime_type
+  #         expiration            = static_files.value.expiration
+  #         require_matching_file = static_files.value.require_matching_file
+  #         application_readable  = static_files.value.application_readable
+  #       }
+  #     }
+  #   }
+  # }
 
   dynamic "libraries" {
     for_each = var.libraries == null ? [] : var.libraries
